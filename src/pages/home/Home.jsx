fa4a7e4f3a2d48c30bd6b42d9b19f3a33673e09e
Mustapha Al-Amin Oluwatimmilehin <110484508+timilehin223@users.noreply.github.com>
2025-11-18 19:38:11 +0000

    Added the LogIn, SignUp and Home Pages

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -43,7 +43,7 @@ function Home() {
       <nav className="dashboard-nav">
         <button>Home</button>
         <button>Transactions</button>
-        <button>Budgetts</button>
+        <button>Budgets</button>
         <button>Reports</button>
         <button>Settings</button>
       </nav>
